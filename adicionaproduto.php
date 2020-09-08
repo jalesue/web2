b3e3e7b7ad72018f366f5ded9da77befb6120cc1
jalesueg <70928411+jalesueg@users.noreply.github.com>
2020-09-07 23:34:54 -0300

    Add files via upload

---
+++ adicionaproduto.php
@@ -0,0 +1,7 @@
+<?php include("cabecalho.php");?>
+<?php
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+?>
+<p class="alert alert-success">Produto <?= $nome;?>, <?= $preco;?> adicionado com sucesso!</p>
+<?php include("rodape.php");?>
---
+++ produtoformulario.php
@@ -0,0 +1,11 @@
+<?php include("cabecalho.php");?>
+<h1>Formulário de Produtos</h1>
+<form action="adicionaproduto.php">
+Nome:
+<input type="text" name="nome"> <br/>
+Preco:
+<input type="number" name="preco"><br/>
+
+<input type="submit" value="Cadastrar">
+</form>
+<?php include("rodape.php");?>
